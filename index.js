7a94371fd1f2f2a3153622befca949f718782d52
unknown <hamai.ismail.dz@gmail.com>
2023-02-19 14:22:13 +0100

    v1

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose'
 
 const app = express();
 
-mongoose.connect("mongodb+srv://<username>:<password>@cluster0.eyhty.mongodb.net/myFirstDatabase?retryWrites=true&w=majority").then(()=>{
+mongoose.connect("mongodb+srv://ismailkun:madarakun0123@hamaiismail.nqf3s2v.mongodb.net/hamaiismail?retryWrites=true&w=majority").then(()=>{
 app.listen(3000, ()=>{console.log('http://localhost:3000')});
 });
 
