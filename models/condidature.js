490920eb0c05ff7406308ae13128805b611b48aa
Riadh Idir <riadhidir5@gmail.com>
2023-02-19 14:57:48 +0100

    v1.8

--- models/condidature.js
+++ models/condidature.js
@@ -1,20 +1,19 @@
-import mongoose, { SchemaType, SchemaTypes } from 'mongoose';
+import mongoose, { SchemaType, SchemaTypes } from "mongoose";
 const { Schema, model } = mongoose;
 
-const condidature = new Schema({
-  condida:{type:SchemaTypes.ObjectId,
-    ref:'condidat',
-required:true},
-  cv: {type:binData,
-    required:true},
-  offre: {type:SchemaTypes.ObjectId,
-    ref:'offer',
-    required:true},
-    sex:{
-        type: String,
-        enum: ['pending', 'accepted','rejected'],
-        default: 'pending'}
-},{timestamps:true});
+const condidatureSchema = new Schema(
+  {
+    condida: { type: SchemaTypes.ObjectId, ref: "Condidat", required: true },
+    cv: { type: binData, required: true },
+    offre: { type: SchemaTypes.ObjectId, ref: "Offer", required: true },
+    sexe: {
+      type: String,
+      enum: ["pending", "accepted", "rejected"],
+      default: "pending",
+    },
+  },
+  { timestamps: true }
+);
 
-const Condidature = model('Condidature', condidature);
+const Condidature = model("Condidature", condidatureSchema);
 export default Condidature;
