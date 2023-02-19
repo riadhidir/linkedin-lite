598594b4018ac2a0142391e7b39863bb9eee8935
unknown <hamai.ismail.dz@gmail.com>
2023-02-19 15:32:20 +0100

    2.1

--- models/Recruteur.js
+++ models/Recruteur.js
@@ -2,23 +2,19 @@ import mongoose from 'mongoose';
 const { Schema, model } = mongoose;
 
 const recruteurSchema = new Schema({
+user:{type:String,required:true},
+
+password:{type:String,required:true},
+
 nom:{
-    type:String,
-    required:true
-},
+    type:String,required:true},
 email:{
-    type:String,
-    required:true
-},
+    type:String,required:true},
 phone:{
     type:String,
-    minKey:5,
-    required:true
-},
+    minKey:5,required:true},
 adresse:{
-    type:String,
-    required:true
-}
+    type:String,required:true}
 
 },{timestamps:true});
 
