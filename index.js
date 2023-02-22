af172d2fabdd25f17bbd717815b5a7632ecc90d7
Riadh Idir <riadhidir5@gmail.com>
2023-02-22 13:30:52 +0100

    v6

--- index.js
+++ index.js
@@ -48,18 +48,18 @@ app.put('/categories/:id',auth(['Admin']),editCategorie);
 app.post('/login',Login);
 // register routs
 app.post('/candidats/register', registerCandidat); 
-app.post('/recruteur/register', registerRecruteur);
+app.post('/recruteurs/register', registerRecruteur);
 
 //get a specific 'candidature' | roles : [recruteur]
 app.get('/candidatures/:id',auth(['Recruteur']), getCandidature);
 //edit a specific 'candidature's state | roles : [recruteur]
-app.post('/recruteurs/candidatures/:id/edit',auth(['Recruteur']), editCandidatureState);
-app.post('/candidatures/:id/edit',[auth(['Candidat']),upload.single('cv')], editCandidature);
+app.put('/recruteurs/candidatures/:id/edit',auth(['Recruteur']), editCandidatureState);
+app.put('/candidatures/:id/edit',[auth(['Candidat']),upload.single('cv')], editCandidature);
 
-//edit all jobs available | roles : [recruteur, candidat]
+//get all jobs available | roles : [recruteur, candidat]
 app.get('/jobs',auth(['Recruteur', 'Candidat']),getAllJobs);
-//edit all jobs intoduced by a recruiter | roles : [recruteur]
-app.get('/recruteur/jobs',auth(['Recruteur']),getMyJobs);
+//get all jobs intoduced by a recruiter | roles : [recruteur]
+app.get('/recruteurs/jobs',auth(['Recruteur']),getMyJobs);
 
 //add a 'candidature' | roles : [candidat]
 app.post('/jobs/:id/apply',[auth(['Candidat']), upload.single('cv')], addCandidature);
